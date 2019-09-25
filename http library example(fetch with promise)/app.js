5a5ed9149aababb12902db740135269ec5753bb9
ilhamva <ilhamva@gmail.com>
2019-09-25 12:21:46 -0400

    initial commit

---
+++ http library example(fetch with promise)/app.js
@@ -0,0 +1,32 @@
+const http = new EasyHTTP;
+
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+
+
+
+const data = {
+    name: "john doe",
+    username: "john",
+    email: "jdoe@gmail.com"
+}
+
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+
+
+
+
